d09d74730174a9d3eb1bafa5d53e69416e78bfcf
MariaJozyane <54179564+MariaJozyane@users.noreply.github.com>
2019-08-16 13:21:28 -0300

    Add files via upload

---
+++ components/deputados.js
@@ -0,0 +1,41 @@
+async function getDeputado() {
+    let template = document.getElementById("template");
+  
+    const profileResponse = await fetch(
+      `https://dadosabertos.camara.leg.br/api/v2/deputados/`
+    );
+  
+    let data = await profileResponse.json();
+    console.log(data.dados);
+
+    const dados = data.dados;
+
+    for (let index = 0; index < dados.length; index++) {
+                const deputados = dados[index];
+
+        template.innerHTML += `
+        <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+                <div class="row" id="card">
+                    <div class="col-md-3">
+                        <img src=${deputados.urlFoto} id="img" />
+                    </div>
+                    <div class="col-md-9">
+                        <p><b>Deputado: </b> ${ deputados.nome }</p>
+                        <p><b>Partido: </b> ${ deputados.siglaPartido } --- ${deputados.siglaUf} </p>
+                        <p><b>Email: </b> <a href="#">${ deputados.email }</a></p>
+                        <a target="_blank" href=${deputados.uri}>Ver gastos</a>
+                    </div>
+                </div>
+        
+    </div>
+        `;
+        
+    }
+  
+  }
